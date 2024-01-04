af9a785291c1a41a5dec4309b7fb8e36398a1681
milap0901 <golmilap27@gmail.com>
2024-01-04 13:04:29 +0530

    math.round

--- src/Home Components/DownloadProgress.js
+++ src/Home Components/DownloadProgress.js
@@ -3,106 +3,120 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 import styles from "./DownloadProgress.module.css";
 
 function DownloadProgress() {
-	const [showData, setShowData] = useState(false);
-	const [progressBar, setProgressbar] = useState({ show: false, progress: 0 });
-	const [checking, setChecking] = useState(false);
-	const [noUpdate, setNoUpdate] = useState(false);
-	const [updateComplete, setUpdateComplete] = useState(false);
+  const [showData, setShowData] = useState(false);
+  const [progressBar, setProgressbar] = useState({ show: false, progress: 0 });
+  const [checking, setChecking] = useState(false);
+  const [noUpdate, setNoUpdate] = useState(false);
+  const [updateComplete, setUpdateComplete] = useState(false);
 
-	const quitAndUpdate = async () => {
-		await window.apiKey.request("quitAndRestart", {});
-	};
+  const quitAndUpdate = async () => {
+    await window.apiKey.request("quitAndRestart", {});
+  };
 
-	useEffect(() => {
-		let timeout;
+  useEffect(() => {
+    let timeout;
 
-		window.apiKey.renderedOn("checking", payload => {
-			console.log("checking");
-			setUpdateComplete(false);
-			setShowData(true);
-			setChecking(true);
-		});
+    window.apiKey.renderedOn("checking", (payload) => {
+      console.log("checking");
+      setUpdateComplete(false);
+      setShowData(true);
+      setChecking(true);
+    });
 
-		window.apiKey.renderedOn("updateAwailaible", payload => {
-			setUpdateComplete(false);
-			setShowData(true);
-			setChecking(false);
-			setNoUpdate(false);
-			setProgressbar(prev => ({ ...prev, show: true }));
-		});
+    window.apiKey.renderedOn("updateAwailaible", (payload) => {
+      setUpdateComplete(false);
+      setShowData(true);
+      setChecking(false);
+      setNoUpdate(false);
+      setProgressbar((prev) => ({ ...prev, show: true }));
+    });
 
-		window.apiKey.renderedOn("updateProgress", payload => {
-			setUpdateComplete(false);
-			setShowData(true);
-			setChecking(false);
-			setNoUpdate(false);
-			setProgressbar(prev => ({ ...prev, progress: payload?.percent }));
-		});
+    window.apiKey.renderedOn("updateProgress", (payload) => {
+      setUpdateComplete(false);
+      setShowData(true);
+      setChecking(false);
+      setNoUpdate(false);
+      setProgressbar((prev) => ({ ...prev, progress: payload?.percent }));
+    });
 
-		window.apiKey.renderedOn("updateNotAwailaible", payload => {
-			setUpdateComplete(false);
-			setShowData(true);
-			setChecking(false);
-			setProgressbar({ show: false, progress: 0 });
-			setNoUpdate(true);
+    window.apiKey.renderedOn("updateNotAwailaible", (payload) => {
+      setUpdateComplete(false);
+      setShowData(true);
+      setChecking(false);
+      setProgressbar({ show: false, progress: 0 });
+      setNoUpdate(true);
 
-			timeout = setTimeout(() => {
-				setShowData(false);
-			}, 20000);
-		});
+      timeout = setTimeout(() => {
+        setShowData(false);
+      }, 20000);
+    });
 
-		window.apiKey.renderedOn("updateDownloaded", payload => {
-			setShowData(true);
-			setChecking(false);
-			setProgressbar({ show: false, progress: 100 });
-			setNoUpdate(false);
-			setUpdateComplete(true);
-		});
+    window.apiKey.renderedOn("updateDownloaded", (payload) => {
+      setShowData(true);
+      setChecking(false);
+      setProgressbar({ show: false, progress: 100 });
+      setNoUpdate(false);
+      setUpdateComplete(true);
+    });
 
-		return () => {
-			window.apiKey.rendereOff("updateProgress");
-			window.apiKey.rendereOff("Checking");
-			window.apiKey.rendereOff("updateAwailaible");
-			window.apiKey.rendereOff("updateNotAwailaible");
-            window.apiKey.rendereOff("updateDownloaded");
-			if (timeout) {
-				clearTimeout(timeout);
-			}
-		};
-	}, []);
+    return () => {
+      window.apiKey.rendereOff("updateProgress");
+      window.apiKey.rendereOff("Checking");
+      window.apiKey.rendereOff("updateAwailaible");
+      window.apiKey.rendereOff("updateNotAwailaible");
+      window.apiKey.rendereOff("updateDownloaded");
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, []);
 
-	if (!showData) return null;
+  if (!showData) return null;
 
-	if (checking) return <div className={styles.progressTitle}> checking for update.....</div>;
+  if (checking)
+    return (
+      <div className={styles.progressTitle}> checking for update.....</div>
+    );
 
-	if (noUpdate) return <div className={styles.progressTitle}> Your Application is already running latest version </div>;
+  if (noUpdate)
+    return (
+      <div className={styles.progressTitle}>
+        {" "}
+        Your Application is already running latest version{" "}
+      </div>
+    );
 
-	if (progressBar.show) {
-		return (
-			<div>
-				<div className={styles.progressTitle}>
-					{" "}
-					<div> Update In Progress...</div>
-					<div> {progressBar.progress} %</div>
-				</div>
-				<ProgressBar className={styles.progressBar} animated now={progressBar.progress} variant="danger" />
-			</div>
-		);
-	}
+  if (progressBar.show) {
+    return (
+      <div>
+        <div className={styles.progressTitle}>
+          {" "}
+          <div> Update In Progress...</div>
+          <div> {Math.round(progressBar.progress)} %</div>
+        </div>
+        <ProgressBar
+          className={styles.progressBar}
+          animated
+          now={progressBar.progress}
+          variant="danger"
+        />
+      </div>
+    );
+  }
 
-	if (updateComplete) {
-		return (
-			<div className={styles.container}>
-				<div className={styles.progressTitle}>
-					{" "}
-					<div> Update Downloaded please Restart.</div>
-				</div>
-				<button className={styles.restart} onClick={quitAndUpdate}>
-					Restart
-				</button>
-			</div>
-		);
-	}
+  if (updateComplete) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.progressTitle}>
+          {" "}
+          <div> Update Downloaded please Restart.</div>
+        </div>
+        <button className={styles.restart} onClick={quitAndUpdate}>
+          Restart
+        </button>
+      </div>
+    );
+  }
 }
 
 export default DownloadProgress;
